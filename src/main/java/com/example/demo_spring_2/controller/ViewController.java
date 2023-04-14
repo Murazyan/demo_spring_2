67767bb50cf5b6ab1d46368fceef755ec0db38f3
murazyan <36574856+Murazyan@users.noreply.github.com>
2023-04-14 19:52:02 +0400

    wip example

--- src/main/java/com/example/demo_spring_2/controller/ViewController.java
+++ src/main/java/com/example/demo_spring_2/controller/ViewController.java
@@ -2,21 +2,27 @@ package com.example.demo_spring_2.controller;
 
 import com.example.demo_spring_2.models.User;
 import com.example.demo_spring_2.service.UserService;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
+@Profile("dev")
 @RequestMapping("/view")
-public class ViewController {
+public class ViewController implements InitializingBean {
 
-
-
-    @Autowired
-    @Qualifier("userServiceImpl")
     private UserService userService;
+    @Autowired
+    public ViewController( @Qualifier("userServiceImpl") UserService userService){
+        this.userService = userService;
+        System.out.println("---- ViewController: constructor");
+    }
 
     @GetMapping("/home")
     public String homePage(@RequestParam(value = "name", required = false, defaultValue = "Petros") String name) {
@@ -38,5 +44,20 @@ public class ViewController {
     @ResponseBody
     public User getUsers(){
         return null;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("----- viewController:afterPropertiesSet method ");
+    }
+
+    @PostConstruct
+    public void  postConstruct(){
+        System.out.println("----- postConstruct");
+    }
+
+    @PreDestroy
+    public void destroy(){
+        System.out.println("---- destroy");
     }
 }
